f09cc1c88c7108c3602dbcc7f01ce4f9f9db7b7d
Heuristic Fencepost <h.fencepost@gmail.com>
2010-07-05 21:21:45 -0500

    Implementation of solution for #7 using mutable state.  Sometimes it is a cleaner solution... and I was interested in seeing what a mutable solution would look like in Scala.

---
+++ src/org/fencepost/Euler7.scala
@@ -0,0 +1,21 @@
+package org.fencepost
+
+// Sometimes mutability does allow for a cleaner expression of an algorithm
+object Euler7 {
+
+  def main(args: Array[String]): Unit = {
+
+    // Pre-populate our prime list with some known quantities
+    var primes = List(2,3,5,7,11,13,17,19)
+    var count = 21
+    while (primes.length < 10001) {
+
+      // Use primes.tail instead or primes here; no need to compare to 2
+      // since we're only considering odd candidates.
+      if (primes.tail.filter(count % _ == 0).length == 0)
+        primes = primes ::: List(count)
+      count += 2
+    }
+    println("Prime 10001: " + primes.last)
+  }
+}
