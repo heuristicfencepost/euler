4db78546c3289c7c13351a182a803d9037325831
Heuristic Fencepost <h.fencepost@gmail.com>
2011-02-27 23:31:21 -0600

    Converted candidate pool to odd values only; no point in testing even values for primality.

--- src/org/fencepost/Euler10.scala
+++ src/org/fencepost/Euler10.scala
@@ -5,7 +5,10 @@ import scala.math.floor
 
 object Euler10 {
 
-  def nat(n:Long):Stream[Long] = Stream.cons(n,nat(n + 1))
+  // A stream for odd values starting from a specific value.  We only need to consider
+  // odds since by definition any prime value other than two must be odd.  Adding this
+  // constraint cuts the number of comparisons we must execute by a factor of two.
+  def odds(n:Long):Stream[Long] = Stream.cons(n,odds(n + 2))
 
   // Predicate to test for primality.  Base test is to verify that a candidate isn't evenly
   // divisible into all integers between 2 and the candidate's square root.  As an optimization
@@ -20,7 +23,7 @@ object Euler10 {
 
   // Note that since we're explicitly stating 2 as the first argument to Stream.cons()
   // we now have to start the natural number sequence at 3.
-  lazy val primes:Stream[Long] = Stream.cons(2,nat(3) filter pred)
+  lazy val primes:Stream[Long] = Stream.cons(2,odds(3) filter pred)
 
   def main(args: Array[String]): Unit = {
 
